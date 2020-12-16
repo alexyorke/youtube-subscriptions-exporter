70c1d0d2a0f5ce5774998923aba2063ad86cdff4
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2020-12-16 18:20:09 -0500

    Wait until sidebar is loaded

--- youtube-subscriptions-exporter.js
+++ youtube-subscriptions-exporter.js
@@ -1,10 +1,36 @@
-if (window.location.href !== "https://www.youtube.com/") alert("Please go to youtube.com in order to run this script");
+// async wrapper required for Firefox, otherwise will get an "await is only valid in async function" error
+var start = async function() {
 
-Array.from(document.querySelectorAll("yt-formatted-string")).find(
-    ytTextElement => /Show \d+ more$/.exec(ytTextElement.textContent) !==
-    null).click();
-let items = Array.from(document.querySelectorAll("a#endpoint")).filter(
-    ytTextElement => !ytTextElement.innerHTML.includes(
-        "style-scope yt-icon")).map(elem => elem.title + "\t" + elem.href);
-		
-console.log(items.join("\n"));
+    if (window.location.href !== "https://www.youtube.com/") alert(
+        "Please go to youtube.com in order to run this script");
+
+    // Firefox requires that the slide out is visible to get the list of subscriptions
+    if (getShowMoreElement() === undefined) {
+        // click on hamburger menu to show slide out
+        document.querySelector("#guide-icon").click();
+        while (getShowMoreElement() === undefined) {
+            await new Promise(r => setTimeout(r, 500));
+        }
+    }
+
+    getShowMoreElement().click();
+
+    let subscriptions = Array.from(document.querySelectorAll(
+        "a#endpoint")).filter(
+        ytTextElement => !ytTextElement.innerHTML.includes(
+            "style-scope yt-icon")).map(elem => elem.title + "\t" +
+        elem.href);
+
+    console.log(subscriptions.join("\n"));
+
+    function getShowMoreElement() {
+        return Array.from(document.querySelectorAll(
+            "yt-formatted-string")).find(
+            ytTextElement => /Show \d+ more$/.exec(ytTextElement
+                .textContent) !==
+            null);
+    }
+
+}
+
+start();
