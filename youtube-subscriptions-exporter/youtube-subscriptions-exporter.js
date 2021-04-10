cb6f6012b290a9822873d1a90bc22d53267c8881
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2021-04-10 18:13:24 -0400

    Add google takeout and newpipe formats

--- youtube-subscriptions-exporter/youtube-subscriptions-exporter.js
+++ youtube-subscriptions-exporter/youtube-subscriptions-exporter.js
@@ -1,10 +1,9 @@
 // async wrapper required for Firefox, otherwise will get an "await is only valid in async function" error
-var start = async function() {
-
+var start = async function () {
     if (window.location.href !== "https://www.youtube.com/") {
-		alert("Please go to youtube.com in order to run this script");
-		return;
-	}
+        alert("Please go to youtube.com in order to run this script");
+        return;
+    }
 
     // Firefox requires that the slide out is visible to get the list of subscriptions
     if (getShowMoreElement() === undefined) {
@@ -19,16 +18,77 @@ var start = async function() {
 
     let subscriptions = Array.from(document.querySelectorAll(
         "a#endpoint")).filter(
-        ytTextElement => !ytTextElement.innerHTML.includes(
-            "style-scope yt-icon"));
-	
+            ytTextElement => !ytTextElement.innerHTML.includes(
+                "style-scope yt-icon"));
+
     let textToCopyToClipboard = "";
-	
-    let exportType = "newpipe";
-    if (exportType === "newpipe") {
-	textToCopyToClipboard = JSON.stringify({"app_version":"0.19.8", "app_version_int": 953, "subscriptions": [...subscriptions.map(elem => {"service_id": 0, "url": elem.href, "name": elem.name})]});
+
+    let exportType = "youtube-takeout";
+    if (exportType === "newPipe") {
+        let newPipeSubscriptions = new Object();
+
+        newPipeSubscriptions.app_version = "0.19.8";
+        newPipeSubscriptions.app_version_int = 953;
+        newPipeSubscriptions.subscriptions = subscriptions.map(elem => {
+            let aSubscription = new Object();
+            aSubscription.service_id = 0;
+            aSubscription.url = elem.href;
+            aSubscription.name = elem.title;
+            return aSubscription;
+        });
+
+        textToCopyToClipboard = JSON.stringify(newPipeSubscriptions);
+    } else if (exportType === "youtube-takeout") {
+        let takeoutSubscription = {
+            "contentDetails": {
+                "activityType": "all",
+                "newItemCount": 0,
+                "totalItemCount": 0
+            },
+            "etag": "",
+            "id": "",
+            "kind": "youtube#subscription",
+            "snippet": {
+                "channelId": "",
+                "description": "",
+                "publishedAt": "",
+                "resourceId": {
+                    "channelId": "__CHANNEL_ID_TEMPLATE__",
+                    "kind": "youtube#channel"
+                },
+                "thumbnails": {
+                    "default": {
+                        "url": ""
+                    },
+                    "high": {
+                        "url": ""
+                    },
+                    "medium": {
+                        "url": ""
+                    }
+                },
+                "title": "__TITLE_TEMPLATE__"
+            }
+        };
+
+        let takeoutSubscriptions = subscriptions.map(elem => {
+            let template = JSON.parse(JSON.stringify(takeoutSubscription));
+            let channelId = elem.href.split("/").pop();
+            template.snippet.resourceId.channelId = channelId;
+            template.snippet.title = elem.title;
+
+            const thumbnails = elem.querySelector("#endpoint > tp-yt-paper-item > yt-icon.guide-icon.style-scope.ytd-guide-entry-renderer").getAttribute("disable-upgrade");
+            const thumbnailUrl = JSON.parse(thumbnails).thumbnails[0].url;
+            template.snippet.thumbnails.default.url = thumbnailUrl;
+            template.snippet.thumbnails.high.url = thumbnailUrl;
+            template.snippet.thumbnails.medium.url = thumbnailUrl;
+            return template;
+        });
+        
+        textToCopyToClipboard = JSON.stringify(takeoutSubscriptions);
+
     } else {
-	textToCopyToClipboard = subscriptions.map(elem => elem.title + "\t" + elem.href).join("\n");
+        textToCopyToClipboard = subscriptions.map(elem => elem.title + "\t" + elem.href).join("\n");
     }
 
     copyToClipboard(textToCopyToClipboard);
@@ -39,11 +99,10 @@ var start = async function() {
     function getShowMoreElement() {
         return Array.from(document.querySelectorAll(
             "yt-formatted-string")).find(
-            ytTextElement => /Show \d+ more$/.exec(ytTextElement
-                .textContent) !==
-            null);
+                ytTextElement => /Show \d+ more$/.exec(ytTextElement
+                    .textContent) !==
+                    null);
     }
-
 }
 
 start();
